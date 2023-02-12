aa387cb38a188d044ebc804472ed0d8f2014f223
hemant meena <38214078+hemant250813@users.noreply.github.com>
2023-02-12 22:19:53 +0530

    search feature

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import ToDo from "./componants/todo";
 import { addToDo, getAllToDo, updateToDo, deleteToDo } from "./utils/handleapi";
 
 
+
 function App() {
 
   const [toDo, setToDo] = useState([])
   const [text, setText] = useState("")
   const [isUpdating, setIsUpdating] = useState(false)
   const [toDoId, setToDoId] = useState("")
+
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+    // our api to fetch the search result
+    console.log("search ", searchTerm);
+  };
 
   useEffect(() => {
     getAllToDo(setToDo)
@@ -22,6 +35,37 @@ function App() {
 
   return (
     <div className="App">
+      <h1 className="h2">Search</h1>
+
+<div className="search-container">
+  <div className="search-inner">
+    <input type="text" value={value} onChange={onChange} />
+    <button onClick={() => onSearch(value)}> Search </button>
+  </div>
+  <div className="dropdown">
+    {toDo
+      .filter((item) => {
+        const searchTerm = value.toLowerCase();
+        const fullName = item.text.toLowerCase();
+
+        return (
+          searchTerm &&
+          fullName.startsWith(searchTerm) &&
+          fullName !== searchTerm
+        );
+      })
+      .slice(0, 10)
+      .map((item) => (
+        <div
+          onClick={() => onSearch(item.text)}
+          className="dropdown-row"
+          key={item.text}
+        >
+          {item.text}
+        </div>
+      ))}
+  </div>
+</div>
 
       <div className="container">
 
@@ -60,9 +104,11 @@ function App() {
         </div>
 
       </div>
+     
 
     </div>
   );
 }
 
 export default App;
+
